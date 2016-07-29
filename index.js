b30cd36e9357fe99fc12b9892e4f930b3e8f9056
Santiago Ferreira <san650@gmail.com>
2016-07-29 14:11:28 -0300

    Add "addons" command

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-inspect-addons'
+  name: 'ember-addons-inspector',
+
+  // Indicates ember-cli to watch for file changes in the addon
+  isDevelopingAddon: function() {
+    return true;
+  },
+
+  // Return an array of commands to add to ember-cli
+  includedCommands: function() {
+    return require('./lib/commands');
+  }
 };
